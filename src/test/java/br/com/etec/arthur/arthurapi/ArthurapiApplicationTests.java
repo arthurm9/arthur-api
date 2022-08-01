9e8e3eaf2b46d3312140d889d24cd9cda9ca1080
Arthur Marques de Oliveira <arthurmarquesoliveira001@gmail.com>
2022-08-01 08:56:30 -0300

    create project, update maven (01/08/2022)

---
+++ src/test/java/br/com/etec/arthur/arthurapi/ArthurapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etec.arthur.arthurapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthurapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
