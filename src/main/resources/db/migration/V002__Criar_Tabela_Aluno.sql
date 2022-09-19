d1b0d4a89fa112c20e101e82c9c5cf46b85622ff
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2022-09-19 08:44:34 -0300

    add: inserts into dbmigration criar_tabela_aluno

--- src/main/resources/db/migration/V002__Criar_Tabela_Aluno.sql
+++ src/main/resources/db/migration/V002__Criar_Tabela_Aluno.sql
@@ -6,3 +6,11 @@ CREATE TABLE aluno (
 );
 
 ALTER TABLE aluno ADD CONSTRAINT FK_Aluno_Curso FOREIGN KEY(cursoid) REFERENCES curso(id);
+
+INSERT INTO aluno(nomealuno, cursoid)
+VAlUES ('Arthur Marques de Oliveira', 1),
+       ('João Érik da Silva Crisostomo', 1),
+       ('Lucas Vieira da Silva', 2),
+       ('Matheus Henrique Crispim Cola', 2),
+       ('Pedro Henrique Vieira Ribeiro', 3),
+       ('Vinícius Henrique dos Santos Pinto', 4);
