39b4cfb74de55e92dea75a3fdb2b8abad96e8bea
Arthur Marques de Oliveira <arthurmarquesoliveira001@gmail.com>
2022-11-01 09:48:01 -0300

    new: list method

--- src/main/java/br/com/etec/arthur/arthurapi/resource/CursoResource.java
+++ src/main/java/br/com/etec/arthur/arthurapi/resource/CursoResource.java
@@ -1,8 +1,19 @@
 package br.com.etec.arthur.arthurapi.resource;
 
+import br.com.etec.arthur.arthurapi.model.Curso;
+import br.com.etec.arthur.arthurapi.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
+@RequestMapping("cursos")
 public class CursoResource {
 
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public List<Curso> listarTodosCursos() {
+    }
 }
